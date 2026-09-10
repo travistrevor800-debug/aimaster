54dbefe041798ace57c94d47a89029db159f9053
travistrevor800-debug <travistrevor800@gmail.com>
2026-09-10 22:01:24 +0300

    Update moduleLoader.js

--- moduleLoader.js
+++ moduleLoader.js
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath, pathToFileURL } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const modulesDir = path.join(__dirname, ".");
 
 /**
  * ModuleLoader
